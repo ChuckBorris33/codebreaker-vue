e6fea0d273c857e86fb9556bd4dc38d759d7e4b5
ChuckBorris33 <boris.zalman33@gmail.com>
2019-10-18 00:23:33 +0200

    Update dependencies

--- dist/precache-manifest.14d1027565fd67dabddf9c704d504e2c.js
+++ dist/precache-manifest.14d1027565fd67dabddf9c704d504e2c.js
@@ -1,27 +1,11 @@
-self.__precacheManifest = [
+self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a7485bc3de38a7c2eb9a",
-    "url": "/static/css/app.fec0e615.css"
-  },
-  {
-    "revision": "a7485bc3de38a7c2eb9a",
-    "url": "/static/js/app.2f985054.js"
-  },
-  {
-    "revision": "86f4c03c614d157d71be",
-    "url": "/static/js/chunk-vendors.cedc2c51.js"
-  },
-  {
-    "revision": "ce4652ba98952a334c603fe2af8b99e1",
-    "url": "/index.html"
+    "revision": "28e88ef5136b9f5c74d0211c3a18f66b",
+    "url": "/favicons/android-icon-144x144.png"
   },
   {
     "revision": "ccb90b1d458f2a6b6ccac5add988c4e1",
     "url": "/favicons/android-icon-192x192.png"
-  },
-  {
-    "revision": "28e88ef5136b9f5c74d0211c3a18f66b",
-    "url": "/favicons/android-icon-144x144.png"
   },
   {
     "revision": "dc711a54f37f34f6303c19ef845d68e3",
@@ -36,12 +20,16 @@ self.__precacheManifest = [
     "url": "/favicons/android-icon-72x72.png"
   },
   {
+    "revision": "776d18f56d4d917913ca2240873d996a",
+    "url": "/favicons/android-icon-96x96.png"
+  },
+  {
     "revision": "19b95263fa1c09a07a58413b0e593b89",
     "url": "/favicons/apple-icon-114x114.png"
   },
   {
-    "revision": "776d18f56d4d917913ca2240873d996a",
-    "url": "/favicons/android-icon-96x96.png"
+    "revision": "aa08be9db9f1a45b98a70f095dcc4848",
+    "url": "/favicons/apple-icon-120x120.png"
   },
   {
     "revision": "28e88ef5136b9f5c74d0211c3a18f66b",
@@ -50,6 +38,10 @@ self.__precacheManifest = [
   {
     "revision": "40a7d7c40548fd9e9a15e551b8864128",
     "url": "/favicons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "010c394c727cc9f9db0cdce3ac2ea491",
+    "url": "/favicons/apple-icon-180x180.png"
   },
   {
     "revision": "e8008c1b32c6ef422e2ba6fdcc7bed32",
@@ -69,15 +61,11 @@ self.__precacheManifest = [
   },
   {
     "revision": "ae3900e86d3a17076dbb204008592fc0",
-    "url": "/favicons/apple-icon.png"
-  },
-  {
-    "revision": "ae3900e86d3a17076dbb204008592fc0",
     "url": "/favicons/apple-icon-precomposed.png"
   },
   {
-    "revision": "5c3468c0e22b5310ce129474e63462b7",
-    "url": "/favicons/favicon-32x32.png"
+    "revision": "ae3900e86d3a17076dbb204008592fc0",
+    "url": "/favicons/apple-icon.png"
   },
   {
     "revision": "a0e5c16937cfd701edf6109c24350d09",
@@ -88,8 +76,16 @@ self.__precacheManifest = [
     "url": "/favicons/favicon-16x16.png"
   },
   {
+    "revision": "5c3468c0e22b5310ce129474e63462b7",
+    "url": "/favicons/favicon-32x32.png"
+  },
+  {
     "revision": "776d18f56d4d917913ca2240873d996a",
     "url": "/favicons/favicon-96x96.png"
+  },
+  {
+    "revision": "3620cd3ee00ac6bfd0356421bc37a916",
+    "url": "/favicons/manifest.json"
   },
   {
     "revision": "28e88ef5136b9f5c74d0211c3a18f66b",
@@ -100,27 +96,35 @@ self.__precacheManifest = [
     "url": "/favicons/ms-icon-150x150.png"
   },
   {
-    "revision": "aa08be9db9f1a45b98a70f095dcc4848",
-    "url": "/favicons/apple-icon-120x120.png"
-  },
-  {
-    "revision": "010c394c727cc9f9db0cdce3ac2ea491",
-    "url": "/favicons/apple-icon-180x180.png"
+    "revision": "076243dc657b18ce00545be4f3b32ce1",
+    "url": "/favicons/ms-icon-310x310.png"
   },
   {
     "revision": "38bd1be48eeb537612e01fd7b428b64f",
     "url": "/favicons/ms-icon-70x70.png"
   },
   {
-    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/robots.txt"
-  },
-  {
-    "revision": "076243dc657b18ce00545be4f3b32ce1",
-    "url": "/favicons/ms-icon-310x310.png"
+    "revision": "96708eb2a1dcfbc31580889c97b6d964",
+    "url": "/index.html"
   },
   {
     "revision": "076243dc657b18ce00545be4f3b32ce1",
     "url": "/logo.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3bd6c20423d74970dc06",
+    "url": "/static/css/app.9b7acd08.css"
+  },
+  {
+    "revision": "3bd6c20423d74970dc06",
+    "url": "/static/js/app.c853fb2d.js"
+  },
+  {
+    "revision": "ec4c7b11bf73f2a34827",
+    "url": "/static/js/chunk-vendors.182f0cce.js"
   }
-];
+]);
